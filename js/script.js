ea040f0aa00ddc968c46187cb847f2fa8fd6cc0c
Your NameTanitych <Tanitych@gmail.com>
2021-10-15 14:12:34 +0600

    3 pages left

--- js/script.js
+++ js/script.js
@@ -76,15 +76,50 @@ $('.plus-btn').on('click', function (e) {
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
 
-    if (value < 100) {
+    if (value < 1000) {
         value = value + 1;
     } else {
-        value = 100;
+        value = 1000;
     }
 
     $input.val(value);
 });
 
-$('.like-btn').on('click', function () {
-    $(this).toggleClass('is-active');
+// function resizable(el, factor) {
+//     var int = Number(factor) || 7.7;
+//     function resize() { el.style.width = ((el.value.length + 1) * int) + 'px' }
+//     var e = 'keyup,keypress,focus,blur,change'.split(',');
+//     for (var i in e) el.addEventListener(e[i], resize, false);
+//     resize();
+// }
+// resizable(document.getElementById('txt'), 7);
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        315: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        455: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        991: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        }
+    }
 });
