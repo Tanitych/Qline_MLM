ef0f1a71becdabb9d526b93e447be78729b77e7b
Your NameTanitych <Tanitych@gmail.com>
2021-10-16 01:31:21 +0600

    range slider done

--- Qline_mlm/js/script.js
+++ Qline_mlm/js/script.js
@@ -76,80 +76,15 @@ $('.plus-btn').on('click', function (e) {
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
 
-    if (value < 1000) {
+    if (value < 100) {
         value = value + 1;
     } else {
-        value = 1000;
+        value = 100;
     }
 
     $input.val(value);
 });
 
-// function resizable(el, factor) {
-//     var int = Number(factor) || 7.7;
-//     function resize() { el.style.width = ((el.value.length + 1) * int) + 'px' }
-//     var e = 'keyup,keypress,focus,blur,change'.split(',');
-//     for (var i in e) el.addEventListener(e[i], resize, false);
-//     resize();
-// }
-// resizable(document.getElementById('txt'), 7);
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 30,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        315: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        455: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 25,
-        },
-        991: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-        }
-    }
+$('.like-btn').on('click', function () {
+    $(this).toggleClass('is-active');
 });
-
-///////////////
-let file_array = [];
-
-function scrollBottom() {
-    let element = document.querySelector('.chat-item-body');
-    element.scrollTop = element.scrollHeight - element.clientHeight;
-}
-
-scrollBottom();
-
-$("#chat-file").change(function () {
-    let input_files = document.getElementById('chat-file').files;
-
-    $.each(input_files, function (index, value) {
-        file_array.push(value);
-        $('.preview__container').append(`
-        <div class="preview__content">
-            <div class="img file_img"><img src="img/icons/tip_icon.jpg" alt=""><img class="file-cross delete_file" data-id="${file_array.length - 1}" src="img/icons/delete.jpg" alt=""></div>
-        </div>
-        `);
-    });
-});
-
-$(document.body).on('click', '.delete_file', function () {
-    let file_id = $(this).data('id');
-    delete file_array[file_id];
-    $(this).parent().remove();
-});
-
